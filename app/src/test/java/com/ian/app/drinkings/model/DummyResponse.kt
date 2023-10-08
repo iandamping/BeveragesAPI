851bfd48dbc5fa7f52b8f842b7032454857cf09d
Ian Damping <iandamping92@gmail.com>
2023-10-08 21:16:03 +0700

    - add unit-test for remote data source - add detekt.yml for compose

---
+++ app/src/test/java/com/ian/app/drinkings/model/DummyResponse.kt
@@ -0,0 +1,44 @@
+package com.ian.app.drinkings.model
+
+import com.ian.app.drinkings.data.remote.api.ResponseAlcoholDrink
+
+object DummyResponse {
+    val DUMMY_ALCOHOL: ResponseAlcoholDrink = ResponseAlcoholDrink(
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "a"
+    )
+}
